1ff44b235ca163b98f0c2031bb5d20531fdeba46
kimgabiana <gabianz@yahoo.com>
2020-06-15 10:26:27 +0800

    change port number

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.get('/', (req, res) => {
   res.send({ message: "welcome to summoner's rift" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(5000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
