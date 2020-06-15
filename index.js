5ea62bdbe68feb88d5bf00da9981a463f2028424
kimgabiana <gabianz@yahoo.com>
2020-06-15 09:58:03 +0800

    add base route

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send({ message: "welcome to summoner's rift" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
