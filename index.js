c4f17c961d1e67a940ae9e922a2d3e300675cc94
kimgabiana <gabianz@yahoo.com>
2020-06-16 13:22:13 +0800

    refactor code

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport.service');
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: "welcome to summoner's rift" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/auth.routes')(app);
+
+// app.get('/', (req, res) => {
+//   res.send({ message: "welcome to summoner's rift" });
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
---
+++ services/passport.service.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecrets,
+      callbackURL: '/auth/google/callback',
+    },
+    (accesstoken, refreshToken, profile, done) => {
+      console.log(accesstoken);
+      console.log(profile);
+      // new User({ googleId: profile.id }).save();
+    }
+  )
+);
